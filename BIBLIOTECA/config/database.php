0d9453e77914950ce003d0058be3d0529abecfa1
Damrod14 <erickrodriguez932@gmail.com>
2024-06-12 23:15:17 -0600

    Cambio en la conexion con la BD

--- BIBLIOTECA/config/database.php
+++ BIBLIOTECA/config/database.php
@@ -1,27 +1,30 @@
 <?php
 
-class Database{
+class Database {
 
-    private $hostname = "roundhouse.proxy.rlwy.net";
-    private $database = "railway";
-    private $username = "root";
-    private $password = "ZbVxMUWCajbqYaSSeAXxeVjcRibWrMdO";
-    private $charset = "utf8mb4";
+    private $db_url = "mysql://root:ZbVxMUWCajbqYaSSeAXxeVjcRibWrMdO@roundhouse.proxy.rlwy.net:32848/railway";
 
-    function conectar()
-    {
-        try{
-            $conexion = "mysql:host=" . $this->hostname . ";dbname=" . $this->database . ";charset=" . $this->charset;
+    function conectar() {
+        try {
+            $url = parse_url($this->db_url);
+            
+            $hostname = $url["host"];
+            $database = substr($url["path"], 1);
+            $username = $url["user"];
+            $password = $url["pass"];
+            $port = $url["port"];
+
+            $conexion = "mysql:host=" . $hostname . ";port=" . $port . ";dbname=" . $database . ";charset=utf8mb4";
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_EMULATE_PREPARES => false
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_TIMEOUT => 30 // Tiempo de espera de 30 segundos
             ];
 
-            $pdo = new PDO($conexion, $this->username, $this->password, $options);
+            $pdo = new PDO($conexion, $username, $password, $options);
 
             return $pdo;
-        }
-        catch(PDOException $e){
+        } catch (PDOException $e) {
             echo 'Error de conexión: ' . $e->getMessage();
             exit;
         }
